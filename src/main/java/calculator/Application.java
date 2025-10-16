f4e4cc614faaf0212dd106da604c3e193ca20346
Jiyun82 <202110918@sangmyung.kr>
2025-10-16 16:07:13 +0900

    feat: 기본/커스텀 중 어떤 구분자 사용하는지 판별

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if (Character.isDigit(input.charAt(0))) { // 기본 구분자
+            System.out.println("기본");
+        } else if (input.startsWith("//")) { // 커스텀 구분자
+            System.out.println("커스텀");
+        } else { // 그 외
+            throw new IllegalArgumentException();
+        }
+
     }
 }
