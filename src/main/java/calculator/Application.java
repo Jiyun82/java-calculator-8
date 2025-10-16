e53c7324d1f6692513fce93f60d6d2a6be8baf7f
Jiyun82 <202110918@sangmyung.kr>
2025-10-16 16:16:10 +0900

    feat: 커스텀 구분자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,11 +8,16 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        String sep = " ";
 
         if (Character.isDigit(input.charAt(0))) { // 기본 구분자
             input = input.replace(',', ' ').replace(':', ' ');
         } else if (input.startsWith("//")) { // 커스텀 구분자
-            System.out.println("커스텀");
+            if (input.indexOf("\\n") != 3) {
+                throw new IllegalArgumentException();
+            }
+            sep = input.substring(2, 3);
+            input = input.substring(5);
         } else { // 그 외
             throw new IllegalArgumentException();
         }
