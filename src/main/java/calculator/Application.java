c4db565604478b75e5add1d4dcb761672adf7b36
Jiyun82 <202110918@sangmyung.kr>
2025-10-17 19:38:28 +0900

    feat: 입력이나 커스텀 구분자 뒤의 값이 없는 경우 0 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,25 +12,30 @@ public class Application {
         String sep = " ";
         double sum = 0;
 
-        if (Character.isDigit(input.charAt(0))) { // 기본 구분자
-            input = input.replace(',', ' ').replace(':', ' ');
-        } else if (input.startsWith("//")) { // 커스텀 구분자
-            if (input.indexOf("\\n") != 3) {
+        if (input.length() > 0) {
+            if (Character.isDigit(input.charAt(0))) { // 기본 구분자
+                input = input.replace(',', ' ').replace(':', ' ');
+            } else if (input.startsWith("//")) { // 커스텀 구분자
+                if (input.indexOf("\\n") != 3) {
+                    throw new IllegalArgumentException();
+                }
+                sep = input.substring(2, 3);
+                input = input.substring(5);
+            } else { // 그 외
                 throw new IllegalArgumentException();
             }
-            sep = input.substring(2, 3);
-            input = input.substring(5);
-        } else { // 그 외
-            throw new IllegalArgumentException();
-        }
 
-        // split 해서 더함
-        String[] parts = input.split(Pattern.quote(sep));
-        for (String part : parts) {
-            if (!isNumberic(part)) {
-                throw new IllegalArgumentException();
+            // split 해서 더함
+            if (input.length() > 0) {
+                String[] parts = input.split(Pattern.quote(sep));
+                for (String part : parts) {
+                    if (!isNumberic(part)) {
+                        throw new IllegalArgumentException();
+                    }
+                    sum += Double.parseDouble(part);
+                }
             }
-            sum += Double.parseDouble(part);
+
         }
 
         if (sum == Math.floor(sum)) {
